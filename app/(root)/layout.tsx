dae1551dfbf648af8c3fe8b088a9b19327387881
Lenin Capa <leviskp87@gmail.com>
2025-09-02 17:54:32 -0500

    add basic queries

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -18,13 +18,13 @@ export default async function HomeLayout({
       <UserProvider user={user}>
          <div className="relative">
             <div className="absolute inset-x-0 top-0 w-full h-[450px] sm:h[500px] md:h-[550px] lg:h-[800px] -z-10 pointer-events-none">
-               <StarsBackground className="w-full h-full" />
+               {/* <StarsBackground className="w-full h-full" /> */}
             </div>
             <Navbar />
             <main>
                {children}
             </main>
-            <Footer />
+            {/* <Footer /> */}
          </div>
       </UserProvider>
    );
---
+++ app/(root)/units/page.tsx
@@ -0,0 +1,5 @@
+export default function PageUnits() {
+  return (
+    <div>Hi</div>
+  );
+}
