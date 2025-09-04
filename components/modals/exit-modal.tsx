16ccada217d602da01085c5c95f36a596d127497
Angel Capa <ricardocmdn@gmail.com>
2025-09-04 17:07:48 -0500

    add modal

---
+++ components/modals/exit-modal.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { useExitModal } from "@/store/use-exit-modal";
+
+export const ExitModal = () => {
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+  const { isOpen, close } = useExitModal();
+
+  useEffect(() => setIsClient(true), []);
+
+  if (!isClient) return null;
+
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogOverlay className="fixed inset-0 bg-black/50 z-[199]" />
+      <DialogContent className="max-w-md z-[200]">
+        <DialogHeader>
+          <DialogTitle className="text-center text-2xl font-bold">
+            Wait, don&apos;t go!
+          </DialogTitle>
+
+          <DialogDescription className="text-center text-base">
+            You&apos;re about to leave the lesson. Are you sure?
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter className="mb-4">
+          <div className="flex w-full flex-col gap-y-4">
+            <Button
+              // variant="primary"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              Keep learning
+            </Button>
+
+            <Button
+              // variant="dangerOutline"
+              className="w-full"
+              size="lg"
+              onClick={() => {
+                close();
+                router.push("/");
+              }}
+            >
+              End session
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
