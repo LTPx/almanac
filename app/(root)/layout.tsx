9e68314a2c06b99841e70e975a7fe0c2b186d867
Lenin Capa <leviskp87@gmail.com>
2025-11-12 16:18:39 -0500

    move ads to main layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,6 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { UserProvider } from "@/context/UserContext";
 import FooterNav from "@/components/footer-nav";
-import DevGoogleAdManager from "@/components/dev-google-admanager";
 
 export default async function HomeLayout({
   children
@@ -28,7 +27,6 @@ export default async function HomeLayout({
         <Navbar />
         <main>{children}</main>
         <FooterNav />
-        <DevGoogleAdManager />
       </div>
     </UserProvider>
   );
