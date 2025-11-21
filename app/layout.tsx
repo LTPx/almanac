4cf64e13d4ea7161ffd7b0ed22524c692ea1f269
Lenin Capa <leviskp87@gmail.com>
2025-11-20 22:29:56 -0500

    move adsense in components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "@/components/ui/sonner";
 import ClientWrapper from "@/components/client-wrapper";
+import AdSense from "@/components/adSense";
 
 const notoSansKR = Noto_Sans_KR({
   weight: ["300", "400", "500", "700"],
@@ -30,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={notoSansKR.variable} suppressHydrationWarning>
+      <head>
+        <AdSense pId="1890321786950620" />
+      </head>
       <body className="antialiased" suppressHydrationWarning>
         <NextTopLoader showSpinner={false} height={6} color="#000000" />
         <Toaster richColors position="top-right" />
